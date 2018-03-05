6deda36977efbcba55b497e1e3989f68c6eac56c
ozkarta <ozbegi1@gmail.com>
2018-03-05 21:18:14 +0400

    business model is added

--- api/v1/model/business.model.js
+++ api/v1/model/business.model.js
@@ -1,6 +1,15 @@
 let mongoose = require('mongoose');
-
+let LocalizationString = require('./types/localization.type');
+let Address = require('./types/address.type');
 let businessSchema = new mongoose.Schema({
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  businessDisplayName: LocalizationString,
+  businessType: [],
+  businessCategories: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+  identificationCode: {},
+  registrationCode: {},
+  address: Address,
+  stores: [{type: mongoose.Schema.Types.ObjectId, ref: 'Store'}]
 }, {
   timestamps: true
 });
--- api/v1/model/user.model.js
+++ api/v1/model/user.model.js
@@ -5,8 +5,10 @@ let userSchema = new mongoose.Schema({
   lastName: {type: String, trim: true},
   email: {type: String, trim: true},
   passwordHash: {type: String},
-  role: {type: String, enum: ['buyer', 'seller','seller-sub-user', 'owner', 'developer']}
+  role: {type: String, enum: ['buyer', 'seller','seller-sub-user', 'owner', 'developer']},
 
+  // Only if role = seller
+  business: {type: mongoose.Schema.Types.ObjectId, ref: 'Business'}
   // address : {
   //   street: {type: String, trim: true},
   //   city: {type: String, trim: true},
